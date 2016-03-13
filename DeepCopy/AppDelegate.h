a964eabb03e0b99575d78b4bbd2adc1220a5eeaa
Better-Jiao <enjoylifejiaoxx@sina.com>
2016-03-13 11:16:14 +0800

    Initial Commit

---
+++ DeepCopy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeepCopy
+//
+//  Created by 焦星星 on 16/3/13.
+//  Copyright © 2016年 jxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeepCopy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeepCopy
+//
+//  Created by 焦星星 on 16/3/13.
+//  Copyright © 2016年 jxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
